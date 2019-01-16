a279eb217456b1cf016e0d0dc7fb3246d231dfc4
Jakob Fischer <fischer.jakob@gmail.com>
2019-01-16 15:07:09 +0100

    completed exercise 34 and 35

--- week2-034.Factorial/src/Factorial.java
+++ week2-034.Factorial/src/Factorial.java
@@ -1,16 +1,22 @@
-import java.util.Scanner;
 
+import java.util.Scanner;
+ 
 public class Factorial {
+ 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         int number = Integer.parseInt(reader.nextLine());
         int i = 1;
+        int result = 1;
         while (i <= number) {
-        if (number == 0) {System.out.println(1);}
-        else {
-            number = i * (i-1);
-        i++;
+            if (!(number == 0)) {
+                result *= i;
+                i++;
+            } else {
+                result = 1;
+            }
         }
-        System.out.println(number);}
+        System.out.println(result);
     }
 }
+ 
--- week2-035.SumOfThePowers/src/SumOfThePowers.java
+++ week2-035.SumOfThePowers/src/SumOfThePowers.java
@@ -1,10 +1,19 @@
-
+ 
 import java.util.Scanner;
-
+ 
 public class SumOfThePowers {
-
+ 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
+        int number = Integer.parseInt(reader.nextLine());
+        int result = 0; 
+        int i = 0;
+        while (i <= number) {
+            
+            result += (int) Math.pow(2, i);
+            i++;
+        
+        }
+        System.out.println(result);
     }
 }
