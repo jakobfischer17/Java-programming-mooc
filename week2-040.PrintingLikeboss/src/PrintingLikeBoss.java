644ee6aa85c3727ffac65a9668c25665834e3bc5
Jakob Fischer <fischer.jakob@gmail.com>
2019-01-28 20:23:31 +0100

    completed ex 40 and 41

--- week2-040.PrintingLikeboss/src/PrintingLikeBoss.java
+++ week2-040.PrintingLikeboss/src/PrintingLikeBoss.java
@@ -1,55 +1,54 @@
-
 public class PrintingLikeBoss {
-
+ 
     // copy or rewrite the method of Assignment 39.1 here
     public static void printStars(int amount) {
-        int i = 0;
-        while (i < amount) {
+        int i = 1;
+        while (i <= amount) {
             System.out.print("*");
             i++;
         }
         System.out.print("\n");
     }
-
+ 
     public static void printWhitespaces(int amount) {
         // 40.1
         int i = 1;
-        while (i < amount) {
+        while (i <= amount) {
             System.out.print(" ");
             i++;
-
+ 
         }
     }
-
+ 
     public static void printTriangle(int size) {
         // 40.2
-        int i = 0;
-        while (i < size) {
+        int i = 1;
+        while (i <= size) {
             printWhitespaces(size - i);
             printStars(i);
             i++;
         }
     }
-
+ 
     public static void xmasTree(int height) {
         // 40.3
         int i = 1;
         int counter = 1;
-        while (i < height) {
+        while (i <= height) {
             printWhitespaces(height - i);
             printStars(counter);
             i++;
             counter = counter + 2;
         }
-        printWhitespaces(((counter - 1) / 2) - 1);
+        printWhitespaces(((counter - 1) / 2) -2);
         printStars(3);
-        printWhitespaces(((counter - 1) / 2) - 1);
+        printWhitespaces(((counter - 1) / 2) -2);
         printStars(3);
     }
-
+ 
     public static void main(String[] args) {
         // Tests do not use main, yo can write code here freely!
-
+ 
         printTriangle(5);
         System.out.println("---");
         printTriangle(1);
--- week2-041.GuessingNumberGame/src/GuessingNumberGame.java
+++ week2-041.GuessingNumberGame/src/GuessingNumberGame.java
@@ -1,19 +1,35 @@
-
+ 
 import java.util.Random;
 import java.util.Scanner;
-
+ 
 public class GuessingNumberGame {
-
+ 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         int numberDrawn = drawNumber();
-
-        // program your solution here. Do not touch the above lines!
         
+        // program your solution here. Do not touch the above lines!
+        int i = 1;
+        while (true) {
+            
+            System.out.println("Guess a number: ");
+            int number = Integer.parseInt(reader.nextLine());
+            if (number < numberDrawn) {
+                System.out.println("The number is greater, guesses made: " + i);
+                i++;
+            } else if (number > numberDrawn) {
+                System.out.println("The number is lesser, guesses made: " + i);
+                i++;
+            } else if (number == numberDrawn) { System.out.println("Congratulations, your guess is correct!");
+            break;
+            }
+ 
+        }
     }
-
+ 
     // DO NOT MODIFY THIS!
     private static int drawNumber() {
         return new Random().nextInt(101);
     }
 }
+ 
