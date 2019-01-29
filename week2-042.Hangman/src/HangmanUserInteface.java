62193a90bda1e88ffbff52ed41eb7045d41d9a1a
Jakob Fischer <fischer.jakob@gmail.com>
2019-01-29 15:04:55 +0100

    completed ex 42

--- week2-042.Hangman/src/HangmanUserInteface.java
+++ week2-042.Hangman/src/HangmanUserInteface.java
@@ -16,7 +16,21 @@ public class HangmanUserInteface {
         System.out.println("");
 
         // PROGRAM YOUR SOLUTION HERE
-
+        while (hangman.gameOn()) {
+            System.out.println("Type a command:");
+            String command = reader.nextLine();
+            if (command.contains("quit")) {
+                break;
+            } else if (command.contains("status")) {
+                hangman.printStatus();
+            } else if (command.length() == 1) {
+                hangman.guess(command);
+            } else if (command.isEmpty()) {
+                printMenu();
+            }
+            hangman.printMan();
+            hangman.printWord();
+        }
 
         System.out.println("Thank you for playing!");
     }
