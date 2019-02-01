9846b0f555b3f0e2bcb4ee5d11e3f6a90939be41
Jakob Fischer <fischer.jakob@gmail.com>
2019-02-01 16:02:07 +0100

    exercise 51 and 52 completed

--- week3-051.SeparatingCharacters/src/SeparatingCharacters.java
+++ week3-051.SeparatingCharacters/src/SeparatingCharacters.java
@@ -5,6 +5,14 @@ public class SeparatingCharacters {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.println("Type a name: ");
+        System.out.println("Type your name: ");
+        String name = reader.nextLine();
+        int i = 0;
+        int j = 1;
+        while (i <= name.length() -1) {
+            System.out.println(j + ". character: " + name.charAt(i));
+            i++;
+            j++;
+        }
     }
 }
