5f4a83327449fe3a61f89304c7e8743edae84527
Jakob Fischer <fischer.jakob@gmail.com>
2019-02-10 19:00:52 +0100

    completed ex 55

--- week3-055.WordInsideWord/src/WordInsideWord.java
+++ week3-055.WordInsideWord/src/WordInsideWord.java
@@ -9,14 +9,11 @@ public class WordInsideWord {
         String fword = reader.nextLine();
         System.out.println("Type the second word: ");
         String sword = reader.nextLine();
-        int index = fword.indexOf(sword);
-
-        if (index == -1) {
-            System.out.println("The word '" + sword + "' is not found in the word '" + fword + "'.");
-        } else {
-            System.out.println("The word '" + fword.substring(index) + "' is found in the word '" + fword + "'."
-            );
+        int index = fword.indexOf(sword); //index value
+        System.out.println(index);
+        if (index != -1) {
+        System.out.println("the word '" + sword + "' is found in the word '" + fword + "'.");}
+        else {System.out.println("the word '" + sword + "' is not found in the word '" + fword + "'.");}
         }
-
     }
-}
+
