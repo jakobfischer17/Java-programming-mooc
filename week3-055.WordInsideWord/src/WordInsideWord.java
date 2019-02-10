637814efcf15930550cf0a39c193314fa9dd4e21
Jakob Fischer <fischer.jakob@gmail.com>
2019-02-10 17:06:02 +0100

    ex 54 partly

--- week3-055.WordInsideWord/src/WordInsideWord.java
+++ week3-055.WordInsideWord/src/WordInsideWord.java
@@ -5,5 +5,18 @@ public class WordInsideWord {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.println("Type the first word: ");
+        String fword = reader.nextLine();
+        System.out.println("Type the second word: ");
+        String sword = reader.nextLine();
+        int index = fword.indexOf(sword);
+
+        if (index == -1) {
+            System.out.println("The word '" + sword + "' is not found in the word '" + fword + "'.");
+        } else {
+            System.out.println("The word '" + fword.substring(index) + "' is found in the word '" + fword + "'."
+            );
+        }
+
     }
 }
